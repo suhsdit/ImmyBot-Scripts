d17624b6ffcdb4f8aa91fd929b956d09292c42c0
Jesse Geron <67440789+jgeron-suhsd@users.noreply.github.com>
2023-03-08 21:52:09 +0000

    added links to files on github

--- Arduino IDE/Arduino IDE Combined Configuration Script.ps1
+++ Arduino IDE/Arduino IDE Combined Configuration Script.ps1
@@ -3,27 +3,8 @@ $configPath = "C:\Program Files\arduino-ide\Appdata\arduino-cli.yaml"
 $localDataDir = "C:\Program Files\arduino-ide\Appdata\Local\Arduino15"
 $localDownloadsDir = "C:\Program Files\arduino-ide\Appdata\Local\Arduino15\staging"
 $userLibrariesDir = "$env:USERPROFILE\Documents\Arduino"
-
-$batchFile = @"
-@echo off
-setlocal
-
-set "CONFIG_NAME=arduino-cli.yaml"
-set "ARDUINO_CLI=C:\Program Files\arduino-ide\resources\app\node_modules\arduino-ide-extension\build\arduino-cli.exe"
-set "APPDATALOCAL_DIR=C:\Program Files\arduino-ide\Appdata"
-set "CONFIG_USER_DIR=%userprofile%\.arduinoIDE"
-set "CONFIG_USER_FILE=%CONFIG_USER_DIR%\%CONFIG_NAME%"
-set "CONFIG_BLANK_FILE=%APPDATALOCAL_DIR%\%CONFIG_NAME%"
-
-if not exist "%CONFIG_USER_DIR%" mkdir "%CONFIG_USER_DIR%"
-if not exist "%CONFIG_USER_FILE%" xcopy /I /Q /H /R /K /Y /S /E "%CONFIG_BLANK_FILE%" "%CONFIG_USER_DIR%"
-attrib +h "%CONFIG_USER_DIR%"
-
-"%ARDUINO_CLI%" config set directories.data      "%APPDATALOCAL_DIR%\Local\Arduino15"         --config-file "%CONFIG_USER_FILE%" "%ARDUINO_CLI%" config set directories.downloads "%APPDATALOCAL_DIR%\Local\Arduino15\staging" --config-file "%CONFIG_USER_FILE%"
-start "" "C:\Program Files\arduino-ide\Arduino IDE.exe"
-
-exit /b 0
-"@
+$batchFile = 'https://raw.githubusercontent.com/suhsdit/ImmyBot-Scripts/main/Arduino%20IDE/Arduino%20IDE.bat'
+$yamlFile = 'https://raw.githubusercontent.com/suhsdit/ImmyBot-Scripts/main/Arduino%20IDE/arduino-cli.yaml'
 
 function Test-ArduinoConfig {
     # Test if the Arduino IDE is properly configured for each user on startup

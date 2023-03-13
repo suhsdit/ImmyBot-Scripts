941838ee43ba0b120849745b4db4825364e65670
Jesse Geron <67440789+jgeron-suhsd@users.noreply.github.com>
2023-03-13 11:18:55 -0700

    remove config bat files

--- Arduino IDE/config1.bat
+++
@@ -1,4 +0,0 @@
-# The folder (with configuration file) is copied/created with the commands:
-XCOPY "Appdata" "C:\Program Files\arduino-ide" /I/Q/H/R/K/Y/S/E
-ICACLS "C:\Program Files\arduino-ide\Appdata" /t /grant Everyone:(OI)(CI)M
-ATTRIB +h "C:\Program Files\arduino-ide\Appdata"
--- Arduino IDE/config2.bat
+++
@@ -1,4 +0,0 @@
-# Install the core boards using configuration file:
-SET "PATH_ARDUINO_CLI=C:\Program Files\arduino-ide\resources\app\node_modules\arduino-ide-extension\build\arduino-cli.exe"
-SET "PATH_ARDUINO_CLI_CONFIG=C:\Program Files\arduino-ide\Appdata\arduino-cli.yaml"
-START /WAIT "" "%PATH_ARDUINO_CLI%" core install arduino:avr     --config-file "%PATH_ARDUINO_CLI_CONFIG%"

3f86f2a8564e9ce3632af949be0c80a930ca5ad4
Jesse Geron <67440789+jgeron-suhsd@users.noreply.github.com>
2023-04-19 14:42:07 -0700

    Update Arduino IDE.bat

--- Arduino IDE/Arduino IDE.bat
+++ Arduino IDE/Arduino IDE.bat
@@ -15,8 +15,6 @@ ATTRIB +h "%CONFIG_USER_DIR%"
 "%ARDUINO_CLI%" config set directories.data      "%APPDATALOCAL_DIR%\Local\Arduino15"         --config-file "%CONFIG_USER_FILE%"
 "%ARDUINO_CLI%" config set directories.downloads "%APPDATALOCAL_DIR%\Local\Arduino15\staging" --config-file "%CONFIG_USER_FILE%"
 
-set update.check=false
-
 START "" "C:\Program Files\arduino-ide\Arduino IDE.exe"
 
 EXIT /B 0
